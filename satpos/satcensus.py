9d0998805fe547a5dd089936fb97f5b6e8b37227
David DeBoer <ddeboer@berkeley.ed>
2020-11-28 22:21:09 -0800

    counts

--- satpos/satcensus.py
+++ satpos/satcensus.py
@@ -1,9 +1,12 @@
+from argparse import Namespace
+
+
 def find_viewable(loc, rng=None, trackfilelist='ls.out'):
     from satpos import sattrack
     from os.path import join
     viewable = open('viewable.out', 'w')
     notviewable = open('notviewable.out', 'w')
-    count_geo = 0
+    count = Namespace(leo=0, meo=0, geo=0, deep=0, other=0, viewable=0, notviewable=0)
     with open(trackfilelist, 'r') as fp:
         i = 0
         for line in fp:
@@ -13,9 +16,16 @@ def find_viewable(loc, rng=None, trackfilelist='ls.out'):
             s = sattrack.Track(join('output', line.strip()))
             s.location(loc)
             s.view()
-            if s.period > 1420.0:
-                print(f"{s.viewable}  GEO+: {s.period}, {s.sublon}")
-                count_geo += 1
+            if s.period > 1500.0:
+                count.deep += 1
+            elif s.period < 1450.0 and s.period > 1420.0:
+                count.geo += 1
+            elif s.period < 1000.0 and s.period > 500.0:
+                count.meo += 1
+            elif s.period < 200.0:
+                count.leo += 1
+            else:
+                count.other += 1
             try:
                 szamin = s.za.min()
                 szamax = s.za.max()
@@ -25,12 +35,21 @@ def find_viewable(loc, rng=None, trackfilelist='ls.out'):
             pline = f"{line.strip()}|{s.scname},{s.satnum},{s.period},{s.sublon},{szamin},{szamax}"
             if s.viewable:
                 print(pline, file=viewable)
+                count.viewable += 1
             else:
                 print(pline, file=notviewable)
+                count.notviewable += 1
             i += 1
-    print(f"Total geo+: {count_geo}")
     viewable.close()
     notviewable.close()
+
+    print(f"LEO: {count.leo}")
+    print(f"MEO: {count.meo}")
+    print(f"GEO: {count.geo}")
+    print(f"DEEP: {count.deep}")
+    print(f"OTHER: {count.other}")
+    print(f"VIEWABLE: {count.viewable}")
+    print(f"NOTVIEWABLE: {count.notviewable}")
 
 
 def generate_check_all(fname, tot):
